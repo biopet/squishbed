37422424938651dd37e9af1ed268af37fcc08138
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-10-10 09:08:21 +0200

    Reformat

--- src/main/scala/nl/biopet/tools/squishbed/SquishBed.scala
+++ src/main/scala/nl/biopet/tools/squishbed/SquishBed.scala
@@ -10,7 +10,8 @@ object SquishBed extends ToolCommand {
       parser.parse(args, Args()).getOrElse(throw new IllegalArgumentException)
 
     if (!cmdArgs.input.exists)
-      throw new IllegalStateException("Input file not found, file: " + cmdArgs.input)
+      throw new IllegalStateException(
+        "Input file not found, file: " + cmdArgs.input)
 
     logger.info("Start")
 
@@ -20,13 +21,15 @@ object SquishBed extends ToolCommand {
     logger.info(s"Total bases: $length")
     logger.info(s"Total bases on reference: $refLength")
     logger.info("Start squishing")
-    val squishBed = records.squishBed(cmdArgs.strandSensitive, cmdArgs.nameSensitive).sorted
+    val squishBed =
+      records.squishBed(cmdArgs.strandSensitive, cmdArgs.nameSensitive).sorted
     logger.info("Done squishing")
     val squishLength = squishBed.length
     val squishRefLength = squishBed.combineOverlap.length
     logger.info(s"Total bases left: $squishLength")
     logger.info(s"Total bases left on reference: $squishRefLength")
-    logger.info(s"Total bases removed from ref: ${refLength - squishRefLength}")
+    logger.info(
+      s"Total bases removed from ref: ${refLength - squishRefLength}")
     squishBed.writeToFile(cmdArgs.output)
 
     logger.info("Done")
