c8b5780226f413da78aa127274006f748eba8c11
pjvan_thof <pjrvanthof@gmail.com>
2018-02-15 15:50:13 +0100

    auto format

--- src/test/scala/nl/biopet/tools/squishbed/SquishBedTest.scala
+++ src/test/scala/nl/biopet/tools/squishbed/SquishBedTest.scala
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2014 Biopet
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package nl.biopet.tools.squishbed
 
 import java.io.File
@@ -26,7 +47,8 @@ class SquishBedTest extends ToolTest[Args] {
       ))
     overlapList.writeToFile(intputFile)
 
-    SquishBed.main(Array("-i", intputFile.getAbsolutePath, "-o", outputFile.getAbsolutePath))
+    SquishBed.main(
+      Array("-i", intputFile.getAbsolutePath, "-o", outputFile.getAbsolutePath))
 
     val output = BedRecordList.fromFile(outputFile)
 
@@ -47,7 +69,11 @@ class SquishBedTest extends ToolTest[Args] {
     intputFile.delete()
     outputFile.delete()
     intercept[IllegalStateException] {
-      SquishBed.main(Array("-i", intputFile.getAbsolutePath, "-o", outputFile.getAbsolutePath))
+      SquishBed.main(
+        Array("-i",
+              intputFile.getAbsolutePath,
+              "-o",
+              outputFile.getAbsolutePath))
     }.getMessage shouldBe s"Input file not found, file: ${intputFile.getAbsolutePath}"
   }
 
